0f202d5e731d573c90c6a1592ecf722fd15fb972
anandhakumarg <anandhsandy.g@gmail.com>
2023-10-10 03:11:46 +0530

    First commit Add-class

---
+++ src/test/java/wowgit/Maths.java
@@ -0,0 +1,31 @@
+package wowgit;
+
+import wowgit.Maths;
+
+public class Maths {
+	
+	public void add(int a,int b){
+		int result =a+b;
+		System.out.println("Sum of two numbers =" + result);
+		}
+	
+
+	
+
+
+	
+
+		public static void main(String[] args){
+	        Maths obj=new Maths();
+		obj.add(3,0);
+		
+		
+
+	      
+
+		}
+
+}
+
+
+
